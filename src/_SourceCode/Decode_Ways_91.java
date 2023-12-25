84ec56576f354926e31ebb8516cd3013d898850c
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2023-12-25 08:47:04 +0700

    25/12/2023

---
+++ src/_SourceCode/Decode_Ways_91.java
@@ -0,0 +1,29 @@
+package _SourceCode;
+
+public class Decode_Ways_91 {
+	public int numDecodings(String s) {
+		if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+			return 0;
+		}
+
+		int n = s.length();
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; ++i) {
+			int oneDigit = s.charAt(i - 1) - '0';
+			int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+			if (oneDigit != 0) {
+				dp[i] += dp[i - 1];
+			}
+
+			if (10 <= twoDigits && twoDigits <= 26) {
+				dp[i] += dp[i - 2];
+			}
+		}
+
+		return dp[n];
+	}
+}
