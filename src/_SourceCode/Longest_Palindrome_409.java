c8bc530da7c16d9e8905657f3202a8f906b5648c
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-01-18 08:43:57 +0700

    18/1/2024

--- src/_SourceCode/Longest_Palindrome_409.java
+++ src/_SourceCode/Longest_Palindrome_409.java
@@ -1,24 +1,34 @@
 package _SourceCode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Longest_Palindrome_409 {
 	public static int longestPalindrome(String s) {
-		int oddCount = 0;
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for (char ch : s.toCharArray()) {
-			map.put(ch, map.getOrDefault(ch, 0) + 1);
-			if (map.get(ch) % 2 == 1) {
-				oddCount++;
+		/*
+		 * Map int oddCount = 0; Map<Character, Integer> map = new HashMap<Character,
+		 * Integer>(); for (char ch : s.toCharArray()) { map.put(ch,
+		 * map.getOrDefault(ch, 0) + 1); if (map.get(ch) % 2 == 1) { oddCount++; } else
+		 * { oddCount--; } } if (oddCount > 1) { return s.length() - oddCount + 1; }
+		 * 
+		 * return s.length();
+		 */
+
+		int freq[] = new int[128];
+
+		for (char c : s.toCharArray()) {
+			freq[c]++;
+		}
+
+		int maxLength = 0;
+		for (int x : freq) { 
+			if (x % 2 == 0) {
+				maxLength += x; // cong cac ky tu co so luong la chan 
 			} else {
-				oddCount--;
+				maxLength += (x - 1); // cong cac ky tu co so luong la le nhung tru 1 de tro thanh chan
 			}
 		}
-		if (oddCount > 1) {
-			return s.length() - oddCount + 1;
+		if (maxLength < s.length()) { // dc cong them 1 ki tu de no nam chinh giua 
+			maxLength++;
 		}
-		return s.length();
+		return maxLength;
 	}
 
 	public static void main(String[] args) {
