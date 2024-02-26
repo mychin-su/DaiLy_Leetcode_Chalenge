d9069309817c42b2a975de72b7ec5e1bb7c8d3ee
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-02-26 07:53:55 +0700

    26/2/2024

---
+++ src/_SourceCode/Same_TreeS_100.java
@@ -0,0 +1,34 @@
+package _SourceCode;
+
+public class Same_TreeS_100 {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	 public boolean isSameTree(TreeNode p, TreeNode q) {
+	        if(p == null && q == null){
+	            return true;
+	        }
+	        if( p == null || q == null ||p.val != q.val){
+	            return false;
+	        }
+	         return
+		        		isSameTree(p.left, q.left ) && isSameTree(p.right
+		          ,q.right);
+	    }
+}
