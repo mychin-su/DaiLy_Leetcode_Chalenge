1d5bb15f414c82e8815ac364ace4134951a7418c
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-03-25 08:04:28 +0700

    25/3/2024

--- src/_SourceCode/Find_All_Duplicates_in_an_Array_442.java
+++ src/_SourceCode/Find_All_Duplicates_in_an_Array_442.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Find_All_Duplicates_in_an_Array_442 {
 	public static List<Integer> findDuplicates(int[] nums) {
-		List<Integer> list = new ArrayList<Integer>();
-		int count[] = new int[100001];
-		for (int num : nums) {
-			count[num]++;
+		List<Integer> list = new ArrayList<>();
+		int count[] = new int[nums.length + 1];
+		for (int freq : nums) {
+			count[freq]++;
 		}
-		for (int i = 0 ; i < count.length; i ++) {
+		for (int i = 1 ; i <= nums.length; i ++) {
 			if (count[i] == 2) {
 				list.add(i);
 			}
