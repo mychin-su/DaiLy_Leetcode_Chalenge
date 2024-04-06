138d01569df6d017c9d9e53f6affb173a373a96b
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-04-06 08:02:09 +0700

    6/4/2024

--- src/_SourceCode/Make_The_String_Great_1544.java
+++ src/_SourceCode/Make_The_String_Great_1544.java
@@ -2,8 +2,8 @@ package _SourceCode;
 
 public class Make_The_String_Great_1544 {
 	public static String makeGood(String s) {
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (Math.abs(s.charAt(i) - s.charAt(i + 1)) == 32) {
+		for(int i = 0; i < s.length() - 1; i ++){
+			if(Math.abs(s.charAt(i) - s.charAt(i + 1)) == 32 ){
 				return makeGood(s.substring(0, i) + s.substring(i + 2));
 			}
 		}
@@ -11,7 +11,7 @@ public class Make_The_String_Great_1544 {
 	}
 
 	public static void main(String[] args) {
-		String s = "leEeetcode";
+		String s = "aAbBcC";
 		System.out.println(makeGood(s));
 	}
 }
