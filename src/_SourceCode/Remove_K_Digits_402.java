8513babdb314952f47c04a0e91925a3dcaae4eae
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-04-11 18:21:37 +0700

    11/4/2024

--- src/_SourceCode/Remove_K_Digits_402.java
+++ src/_SourceCode/Remove_K_Digits_402.java
@@ -5,23 +5,18 @@ public class Remove_K_Digits_402 {
     if(num.length() == k){
       return "0";
     }
-
     StringBuilder stack = new StringBuilder();
-    for(char digit : num.toCharArray()){
-      while(stack.length() > 0 && k > 0 && stack.charAt(stack.length() - 1)  > digit){
-          stack.setLength(stack.length() - 1);
-          k--;
+    for(char c : num.toCharArray()){
+      while(stack.length() > 0 && k > 0 && stack.charAt(stack.length() - 1) > c){
+        stack.setLength(stack.length() - 1);
+        k--;
       }
-      stack.append(digit);
+      stack.append(c);
     }
-
-    //Remove any remaining digits from the end (neu k van con lon hon 0 thi xoa di cac so con lai o cuoi)
     while(k > 0){
       stack.setLength(stack.length() - 1);
       k--;
     }
-
-    //Remove leading zero ("Xoa cac so o dau")
     int start = 0;
     while(start < stack.length() && stack.charAt(start) == '0'){
       start++;
@@ -33,8 +28,8 @@ public class Remove_K_Digits_402 {
   }
 
   public static void main(String[] args) {
-      String num = "1432219";
-      int k = 3;
+      String num = "10200";
+      int k = 1;
     System.out.println(removeKdigits(num, k));
   }
 }
