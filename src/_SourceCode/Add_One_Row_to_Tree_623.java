02cec84587610d40185aec3b8efcb6acd1bb9198
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-04-17 09:20:54 +0700

    17/4/2024

--- src/_SourceCode/Add_One_Row_to_Tree_623.java
+++ src/_SourceCode/Add_One_Row_to_Tree_623.java
@@ -49,9 +49,12 @@ public class Add_One_Row_to_Tree_623 {
     TreeNode root = new TreeNode(4);
     root.left = new TreeNode(2);
     root.left.left = new TreeNode(3);
-    root.right = new TreeNode(6);
+    root.left.left.left = new TreeNode(9);
     root.left.right = new TreeNode(1);
+    root.left.right.right = new TreeNode(7);
+    root.right = new TreeNode(6);
     root.right.left = new TreeNode(5);
-    System.out.println(addOneRow(root, 1, 2));
+    root.right.right = new TreeNode(10);
+    System.out.println(addOneRow(root, 11, 4));
   }
 }
