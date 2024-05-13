357f6a6917f1c385addf6c18fc3da073af95ffbf
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-05-13 08:56:57 +0700

    13/5/2024

---
+++ src/_SourceCode/Score_After_Flipping_Matrix_861.java
@@ -0,0 +1,29 @@
+package _SourceCode;
+
+public class Score_After_Flipping_Matrix_861 {
+  public static int matrixScore(int[][] grid) {
+    int n = grid.length, m = grid[0].length;
+    int res = (1 << (m - 1)) * n;
+
+    for (int j = 1; j < m; ++j) {
+      int val = 1 << (m - 1 - j);
+      int set = 0;
+
+      for (int i = 0; i < n; ++i) {
+        if (grid[i][j] == grid[i][0]) {
+          set++;
+        }
+      }
+
+      res += Math.max(set, n - set) * val;
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) {
+    int[][] grid = {{0, 0, 1, 1}, {1, 0, 1, 0}, {1, 1, 0, 0}};
+    System.out.println(matrixScore(grid));
+  }
+
+}
