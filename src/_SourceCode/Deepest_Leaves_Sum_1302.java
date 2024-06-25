c77459cd8af7983f01d0f9e761b6bd1364d95752
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-06-25 16:13:27 +0700

    Revert "25/6/2024"
    This reverts commit 46084c2c57cc9728a575633928fa00f921be3e8a.

--- src/_SourceCode/Deepest_Leaves_Sum_1302.java
+++ src/_SourceCode/Deepest_Leaves_Sum_1302.java
@@ -1,5 +1,53 @@
 package _SourceCode;
 
 public class Deepest_Leaves_Sum_1302 {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+      this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right){
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
 
+  static int maxHeight = Integer.MIN_VALUE;
+  static int maxSum = 0;
+  public static int deepestLeavesSum(TreeNode root) {
+    inorderDFS(root, 0);
+    return maxSum;
+  }
+
+  public static void inorderDFS(TreeNode root, int height){
+    if(root == null){
+      return;
+    }
+    height++;
+    inorderDFS(root.right, height);
+    if(root.left == null && root.right == null){ // Node is leaf
+        if(maxHeight < height){
+          maxHeight = height;
+          maxSum = root.val;
+        }else if(maxHeight == height){
+          maxSum = maxSum + root.val;
+        }
+    }
+    inorderDFS(root.left, height);
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(4);
+    root.left.left.left = new TreeNode(7);
+    root.left.right = new TreeNode(5);
+    root.right = new TreeNode(3);
+    root.right.right = new TreeNode(6);
+    root.right.right.right = new TreeNode(8);
+    System.out.println(deepestLeavesSum(root));
+  }
 }
